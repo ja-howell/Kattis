063d31504ac77e50b230f855c56b137bebcb666e
ja-howell <42947215+ja-howell@users.noreply.github.com>
2018-09-26 10:46:40 -0500

    began zamka problem

---
+++ zamka/zamka.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class zamka{
+    public static void main(String [] args){
+        Scanner scnr = new Scanner(System.in);
+        int min = 0;
+        int max = 0;
+        int l = scnr.nextInt();
+        int d = scnr.nextInt();
+        int x = scnr.nextInt();
+
+            }
+
+        }
+    }
+}
