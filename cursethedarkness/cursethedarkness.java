ec5e6b4b3c38840aa32234b17fc92737f8e8015b
ja-howell <42947215+ja-howell@users.noreply.github.com>
2018-12-23 12:14:08 -0600

    complete curse the darkness in java

---
+++ cursethedarkness/cursethedarkness.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class cursethedarkness {
+    public static void main(String [] args) {
+        Scanner kb = new Scanner(System.in);
+        double xBook, yBook, xCandle, yCandle, distance;
+        int testCases, candles;
+        boolean light;
+
+        testCases = kb.nextInt();
+
+        for(int i = 0; i < testCases; ++i) {
+            light = false;
+            xBook = kb.nextDouble();
+            yBook = kb.nextDouble();
+            candles = kb.nextInt();
+                for(int j = 0; j < candles; ++j) {
+                    xCandle = kb.nextDouble();
+                    yCandle = kb.nextDouble();
+                    
+
+                    if(getDistance(xBook, xCandle, yBook, yCandle) <= 8) {
+                        light = true;
+                    }
+                }
+            if(light == true) {
+                System.out.println("light a candle");
+            }
+            else {
+                System.out.println("curse the darkness");
+            }
+
+        }
+
+
+
+    }
+    public static double getDistance(double x1, double x2, double y1, double y2) {
+        double distance = 0.0;
+        distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+        return distance;
+    }
+
+}
